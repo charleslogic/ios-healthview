401b90432c25d92875516461b8c36ef0ca9f0773
Charles Richards <charles76012@yahoo.com>
2026-06-07 15:53:26 -0500

    first run

---
+++ HealthView/HealthView/HealthViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthViewApp.swift
+//  HealthView
+//
+//  Created by Charles on 6/7/26.
+//
+
+import SwiftUI
+
+@main
+struct HealthViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
